5b20ac9c50974fd269a0491aa3320303c2c733d0
Vamsi Krishna V <vamsi@vonectech.com>
2017-07-08 17:41:49 +0530

    Initial Commit

---
+++ src/gitsyncServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace vonec\gitsync;
+
+use Illuminate\Support\ServiceProvider;
+
+class gitsyncServiceProvider extends ServiceProvider
+{
+    /**
+     * Perform post-registration booting of services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register any package services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
